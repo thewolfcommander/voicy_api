6822d50d25364aa8bb917facab9f541e5db379ba
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-07-15 23:15:00 +0530

    Added Chat Approval API

--- voicy/urls.py
+++ voicy/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', include('admin.urls')),
+    path('admin/', include(admin.urls)),
     path('accounts/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
 ]
