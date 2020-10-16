04e04364364d6a340195a4c68080a8d2c0421316
MANJEET KUMAR <43195111+Manjeete@users.noreply.github.com>
2020-10-17 01:45:48 +0530

    adding models to admin controller

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UserProfile,UserProfileFollower,ChatTransactionApprove
 
 # Register your models here.
+admin.site.register(ChatTransactionApprove)
+admin.site.register(UserProfileFollower)
+admin.site.register(UserProfile)
